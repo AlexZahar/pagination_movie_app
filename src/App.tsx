0c5ca39456153323988b55eee7aee94765611cc8
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-08-21 16:20:01 +0200

    Initial project commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Hello there!</h1>
+      <button onClick={() => (alert("Hello world!"))} data-cy="btn-hello">Click me!</button>
+    </div>
+  );
+}
+
+export default App;
